686dc60a99559abe5f720f3325eb17ff84abf210
0x-normal <49248760+0x-normal@users.noreply.github.com>
2025-07-22 02:29:02 +0100

    Update save-score.js

--- api/save-score.js
+++ api/save-score.js
@@ -6,6 +6,15 @@ const pool = new Pool({
 });
 
 export default async function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+// Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
